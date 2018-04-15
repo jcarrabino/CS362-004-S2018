696a649da73e8177ab03fec784441b4ef9240d3d
John Carrabino <jcarrabino13@gmail.com>
2018-04-15 15:46:03 -0400

    Add files via upload

--- dominion/dominion_helpers.h
+++ dominion/dominion_helpers.h
@@ -12,4 +12,14 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
+int playMine(int *currentPlayer, struct gameState *state, int *choice1, int *choice2, int *j, int *handPos);
+
+int playFeast(int *currentPlayer, struct gameState *state, int *temphand, int *choice1, int *x);
+
+int playCouncilRoom(int *currentPlayer, struct gameState *state, int *handPos);
+
+int playAventurer(int *drawntreasure, int *currentPlayer, struct gameState *state, int *temphand, int *z, int *cardDrawn);
+
+int playSmithy(int *currentPlayer, struct gameState *state, int *handPos);
+
 #endif
